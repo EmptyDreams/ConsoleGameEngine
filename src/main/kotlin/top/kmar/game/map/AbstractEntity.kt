4894f52430d10328bc0dbff89709ace9f3b81af4
kmar <minedreams@qq.com>
2023-04-16 17:10:52 +0800

    添加`AbstractEntity`

---
+++ src/main/kotlin/top/kmar/game/map/AbstractEntity.kt
@@ -0,0 +1,31 @@
+package top.kmar.game.map
+
+/**
+ * 内置了一些实现的实体类
+ * @author 空梦
+ */
+abstract class AbstractEntity(
+    x: Int, y: Int, width: Int, height: Int,
+    visible: Boolean, collisible: Boolean
+) : GEntity {
+
+    final override var visible = visible
+        private set
+    final override var collisible = collisible
+        private set
+    final override var x = x
+        private set
+    final override var y = y
+        private set
+    final override var width = width
+        private set
+    final override var height = height
+        private set
+    final override var died = false
+        private set
+
+    override fun beKilled(map: GMap, killer: GEntity) {
+        died = true
+    }
+
+}
