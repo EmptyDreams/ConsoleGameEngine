c66d686cd2521eb739dc6081c5356ae6628a2432
kmar <minedreams@qq.com>
2023-04-16 19:37:51 +0800

    添加 fun 修饰符

--- src/main/kotlin/top/kmar/game/listener/IMousePosListener.kt
+++ src/main/kotlin/top/kmar/game/listener/IMousePosListener.kt
@@ -1,7 +1,7 @@
 package top.kmar.game.listener
 
 /** 鼠标坐标事件 */
-interface IMousePosListener {
+fun interface IMousePosListener {
 
     /** 鼠标移动事件 */
     fun onMove(x: Int, y: Int, oldX: Int, oldY: Int)
